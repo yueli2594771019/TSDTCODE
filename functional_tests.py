0656dbc7655658519b4a58ef2dd790dfd1a52ac1
undefined zy <1982311344@qq.com>
2024-03-28 15:53:35 +0800

    message 6:Functional test checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+from selenium.webdriver.common.by import By
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -15,14 +18,28 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
 
         # 他注意到网页里包含“To-Do”这个词
-        self.assertIn('To-Do', self.browser.title), "browser title was:" + self.browser.title
-        self.fail('Finish the test!')
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+        self.assertIn('To-Do',  header_text)
 
         # 应用有一个输入代办事项的文本输入框
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
+        
         # 他在文本输入框里输入了“Buy flowers"
+        inputbox.send_keys('Buy flowers')
+        
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # 他访问了URL，发现他的待办事项列表还在
-        # 他满意的离开了
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows=table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn('1:Buy flowers',[row.text for row in rows])
+        
+        self.fail('Finish the test!')
 
 if __name__ == '__main__':
     unittest.main()
