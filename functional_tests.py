3f38294131753ba64710624f5e2800e439b0c092
undefined zy <1982311344@qq.com>
2024-04-15 13:57:52 +0800

    message 9: After refactoring the functional_tests

--- functional_tests.py
+++ functional_tests.py
@@ -12,27 +12,47 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # 弹出网页有一个定位输入的网页
-        # 他去看了这个网页的首页
+        # 网页的首页
         self.browser.get('http://localhost:8000')
 
-        # 他注意到网页里包含“To-Do”这个词
+        # 网页里包含“To-Do”这个词
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element(By.TAG_NAME,'h1').text
         self.assertIn('To-Do',  header_text)
 
-        # 应用有一个输入代办事项的文本输入框
+        # 应用有一个输入待办事项的文本输入框
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
+
+        # 输入“Buy flowers”
+        inputbox.send_keys('Buy flowers')
+
+        #按回车键页面更新
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+        self.check_for_row_in_list_table('1: Buy flowers')
+        
+        # 应用又显示一个文本输入框，可以输入其他代办事项
+        # 输入'Give a gift to Lisi'
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Give a gift to Lisi')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        
-        table = self.browser.find_element(By.ID,'id_list_table')
-        rows=table.find_elements(By.TAG_NAME,'tr')
-        self.assertIn('1: Buy flowers',[row.text for row in rows])
-        self.assertIn('2: Give a gift to List',[row.text for row in rows])
-        
+        time.sleep(3)
+
+        #页面更新，显示了两个待办事项
+        self.check_for_row_in_list_table('1: Buy flowers')
+        self.check_for_row_in_list_table('2: Give a gift to Lisi')
+
+        #网站生成了一个唯一的URL
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
