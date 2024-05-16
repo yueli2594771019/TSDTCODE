fe9df07bbf7a282d4f133e07649fc7254ba4605b
undefined zy <1982311344@qq.com>
2024-05-16 16:18:54 +0800

    message 16: form URL of new list by POST

--- lists/tests.py
+++ lists/tests.py
@@ -6,22 +6,6 @@ from django.template.loader import render_to_string
 from lists.models import Item
 
 class ItemModelTest(TestCase):
-    
-    def test_can_save_a_POST_request(self):
-        response = self.client.post('/',data={'item_text': 'A new list item'})
-
-        self.assertEqual(Item.objects.count(),1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_redirects_after_POST(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-new-page/')
-
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
         
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -48,9 +32,26 @@ class HomePageTest(TestCase):
 
 class ListViewTest(TestCase):
     
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-new-page/')
+        self.assertTemplateUsed(response,'list.html')
+        
     def test_displays_all_items(self):
         response = self.client.get('/lists/the-new-page/')
         self.assertTemplateUsed(response,'list.html')
+
+class NewListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/lists/new',data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertRedirects(response, '/lists/the-new-page/')
+        
 
 
 
